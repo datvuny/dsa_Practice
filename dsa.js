26e68a82701f60f565a762625eb99614179f915e
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-03 17:03:43 -0400

    add twoSum

--- dsa.js
+++ dsa.js
@@ -62,3 +62,46 @@ const isAnagram = (s, t)=> {
     return true;
 };
 
+// TWO SUM
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+// Nested for-loops w./ time complexity: O(n^2)
+// const twoSum = (nums, target) => {
+//    for (let i =0; i<nums.length;i++){
+//        for (let j=i+1; j<nums.length;j++){
+//            if (nums[i]+nums[j]== target) return [i,j]
+//        }
+//    }
+//    return []
+// };
+
+// // time complexity: O(n) but hash does not count duplicates ;(
+// const twoSum = (nums, target) => {
+//     const seen = new Set()
+//     for (let i=0; i<nums.length;i++){
+//         let complement = target - nums[i]
+//         if (seen.has(complement)){
+//             return [[...seen].indexOf(complement), i]
+//         }
+//         seen.add(nums[i])
+//     }
+//     return []
+// }
+
+const twoSum = (nums, target) =>{
+    const hashMap = new Map;
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (hashMap.has(complement)) {
+            return [hashMap.get(complement), i]
+        }
+        hashMap.set(nums[i], i);
+    }
+}
+
