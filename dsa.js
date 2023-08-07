c303716029c5cf2584ff4a7f37e5feef2ea53cdb
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-07 17:47:18 -0400

    add productArr

--- dsa.js
+++ dsa.js
@@ -146,3 +146,69 @@ const topKFrequent = (nums, k) => {
     return sortedNumbers.slice(0,k)
 };
 
+// PRODUCT OF ARRAY EXCEPT SELF
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+function productExceptSelf(nums) {
+    const n = nums.length;
+    
+    // Initialize prefix and suffix product arrays
+    const prefixProducts = new Array(n).fill(1);
+    const suffixProducts = new Array(n).fill(1);
+    
+    // Calculate prefix products
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        prefixProducts[i] = prefix;
+        prefix *= nums[i];
+    }
+    
+    // Calculate suffix products
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        suffixProducts[i] = suffix;
+        suffix *= nums[i];
+    }
+    
+    // Calculate answer using prefix and suffix products
+    const answer = [];
+    for (let i = 0; i < n; i++) {
+        answer[i] = prefixProducts[i] * suffixProducts[i];
+    }
+    
+    return answer;
+}
+*/
+
+const productExceptSelf = (nums) => {
+    let result = []
+    let prefix = []
+    let suffix = []
+
+    for (let i=0; i<nums.length; i++){
+        if (i==0){
+            prefix[i] = 1
+        } else {
+            prefix[i] = nums[i-1] * prefix[i-1]
+        }
+    }
+    for (let i=nums.length - 1; i>=0; i--){
+        if (i==nums.length -1){
+            suffix[i] = 1
+        } else {
+            suffix[i] = nums[i+1] * suffix[i+1]
+        }
+    }
+    for (let i=0; i<nums.length; i++){
+        result[i] = prefix[i] * suffix[i]
+    }
+    return result
+};
+
