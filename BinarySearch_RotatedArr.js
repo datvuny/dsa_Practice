e813291d71b338012e2ea16893afe71a67f244df
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-14 17:07:28 -0400

    add binarySearch

--- BinarySearch_RotatedArr.js
+++ BinarySearch_RotatedArr.js
@@ -50,3 +50,42 @@ const findMin = (nums) => {
     return result
 };
 
+
+//      SEARCH TARGET IN ROTATED SORTED ARR     //
+
+/*
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+You must write an algorithm with O(log n) runtime complexity. 
+
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+ */
+
+const search = (nums, target) => {
+    if (nums[0] === target) return 0
+    let l = 0,
+        r = nums.length -1;
+
+    while (l <= r) {
+        let middle = Math.floor(((l + r)/2));
+        if (nums[middle] === target) return middle;
+        
+        if (nums[l] <= nums[middle]) {
+            if (target >= nums[l] && target < nums[middle]){
+                r = middle - 1
+            }   else {
+                l = middle + 1
+            }
+    }   else {
+        if (target > nums[middle] && target <= nums[r]) {
+            l = middle +1
+        } else {
+            r = middle - 1
+        }
+    }
+    }
+    return -1
+};
