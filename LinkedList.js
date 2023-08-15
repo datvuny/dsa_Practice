838a86696c4f00ac2bec987d17d1db2f4bf96a5a
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-14 22:52:07 -0400

    add reorder link

--- LinkedList.js
+++ LinkedList.js
@@ -121,4 +121,60 @@ const hasCycle = (head) => {
     }
     return false;
   };
-  
+
+
+
+//      REORDER LIST    //
+/////////////////////////
+
+/*
+You are given the head of a singly linked-list. The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+Example 1:
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+*/
+
+var reorderList = function(head) {
+    if (!head || !head.next || !head.next.next) {
+        return head;
+    }
+
+    // Find the middle of the linked list
+    let slow = head;
+    let fast = head;
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // Reverse the second half of the linked list
+    let prev = null;
+    let current = slow.next;
+    while (current) {
+        let nextNode = current.next;
+        current.next = prev;
+        prev = current;
+        current = nextNode;
+    }
+    slow.next = null;
+
+    // Merge the two halves alternately
+    let first = head;
+    let second = prev;
+    while (second) {
+        let nextFirst = first.next;
+        let nextSecond = second.next;
+
+        first.next = second;
+        second.next = nextFirst;
+
+        first = nextFirst;
+        second = nextSecond;
+    }
+}
+
