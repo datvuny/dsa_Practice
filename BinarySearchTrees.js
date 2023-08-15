ef785beb6459806a941e424c12a1ed70836ca73f
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-14 23:18:33 -0400

    edit invert bst

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -5,30 +5,40 @@ Example 1:
 Input: root = [4,2,7,1,3,6,9]
 Output: [4,7,2,9,6,3,1]
 */
-/*
-const invertTree = (root) => {
-    if (!root) return null;
-    
-    //swap the left and the right subtrees
-    const temp = root.left;
-    root.left = root.right;
-    root.right = temp;
 
-    // recursively invert the left and right subtrees
-    invertTree(root.left);
-    invertTree(root.right);
-
-    return root
-};
-*/
-
-// ES6
-const invertTree = (root) => {
-  if (root === null) {
-    return null;
-  }
-  
+// Recursion
+function invertTree(root) {
+  if (root == null) return root;
   [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
-  
   return root;
-};
+}
+
+// DFS
+function invertTree(root) {
+  const stack = [root];
+
+  while (stack.length) {
+    const n = stack.pop();
+    if (n != null) {
+      [n.left, n.right] = [n.right, n.left];
+      stack.push(n.left, n.right);
+    }
+  }
+
+  return root;
+}
+
+// BFS
+function invertTree(root) {
+  const queue = [root];
+
+  while (queue.length) {
+    const n = queue.shift();
+    if (n != null) {
+      [n.left, n.right] = [n.right, n.left];
+      queue.push(n.left, n.right);
+    }
+  }
+
+  return root;
+}
