f7ca5ae6d8737800b3f343c003f5dd06ba10f692
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-14 22:11:32 -0400

    add cyclic

--- LinkedList.js
+++ LinkedList.js
@@ -85,3 +85,40 @@ var mergeTwoLists = function(l1, l2) {
     return tempNode.next;
 };
 
+//      LINKED LIST CYCLE   //
+/////////////////////////////
+
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+
+const hasCycle = (head) => {
+    const seenNode = new Set();
+
+    while (head) {
+        if (seenNode.has(head)) return true;
+        seenNode.add(head);
+        head = head.next; // Move to the next node
+    }
+
+    return false; // Return false if we reached the end of the list without finding a cycle
+ */
+
+// Using Fast pointer, and a 'slow pointer'
+const hasCycle = (head) => {
+    let fast = head;
+    while (fast && fast.next) {
+      head = head.next;
+      fast = fast.next.next;
+      if (head === fast) return true;
+    }
+    return false;
+  };
+  
