46e1b3b6772f186ab47fde43484c42c79f992db8
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-14 20:50:01 -0400

    add mergelist

--- LinkedList.js
+++ LinkedList.js
@@ -53,3 +53,35 @@ var reverseList = function(head) {
     return reversedListHead
 };
 
+//      MERGE 2 SORTED LIST     //
+////////////////////////////////
+
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list. 
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+*/
+
+var mergeTwoLists = function(l1, l2) {
+    let tempNode = new ListNode(0, null);
+    let currentNode = tempNode;
+    
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            currentNode.next = l1;
+            l1 = l1.next
+        } else {
+            currentNode.next = l2;
+            l2 = l2.next
+        }
+        currentNode = currentNode.next;
+    }
+    currentNode.next = l1 || l2;
+    
+    return tempNode.next;
+};
+
