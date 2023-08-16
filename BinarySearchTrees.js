5c8345babac11730064787ad722ac0dca157e3b7
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-16 18:53:27 -0400

    add bst lcd

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -216,3 +216,27 @@ function isSameTree (p, q) {
     }
     return (p.val === q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
+
+//    LOWEST COMMON ANCESTOR OF A BST   //
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+Example 1:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+*/
+
+const lowestCommonAncestor = (root, p, q) => {
+  if (!root) {
+      return null; // Base case: empty tree or reached a leaf node
+  }
+
+  if (p.val < root.val && q.val < root.val) {
+      return lowestCommonAncestor(root.left, p, q); // Both nodes are in the left subtree
+  } else if (p.val > root.val && q.val > root.val) {
+      return lowestCommonAncestor(root.right, p, q); // Both nodes are in the right subtree
+  } else {
+      return root; // Found the split point, this is the LCA
+  }
+};
