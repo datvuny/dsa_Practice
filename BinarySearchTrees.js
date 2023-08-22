6a90f2e8a706b2d318d181e7e8ff9826110c8733
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-22 16:46:14 -0400

    add isValid BST

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -276,3 +276,52 @@ const levelOrder = (root) => {
   }
   return result
 };
+
+
+//    isValid BST?    //
+////////////////////////
+
+/*
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+A valid BST is defined as follows:
+The left 
+subtree
+ of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+Input: root = [2,1,3]
+Output: true
+Example 2:
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+*/
+
+// recursive, bsf, divide and conquer
+const isValidBST = (root)=> {
+    
+  return validate(root, -Infinity, Infinity);
+};
+
+
+const validate = (node, lower,upper)=> {
+  
+  if ( node == null ){
+      
+      // empty node or empty tree
+      return true;
+  }
+  
+  if( (lower < node.val) && ( node.val < upper ) ){
+      
+      // check if all tree nodes follow BST rule
+      return validate( node.left, lower, node.val) && validate( node.right, node.val, upper);
+  }else{
+      
+      // early reject when we find violation
+      return false;
+  }
+}
+
