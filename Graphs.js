9303b7bab457a34621b07bd38eb5a88ae2775244
dat vu <91701889+datvuny@users.noreply.github.com>
2023-08-31 14:12:59 -0400

    add graph_clone

--- Graphs.js
+++ Graphs.js
@@ -100,3 +100,73 @@ function numIslands(grid) {
 
  */
 
+//////      CLONE GRAPH     //////
+/*
+Given a reference of a node in a connected undirected graph.
+Return a deep copy (clone) of the graph.
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+Test case format:
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+Example 1:
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+*/
+//---Depth-First Search (DFS)--- 
+const cloneGraph = (node) =>{
+    if (!node) {
+        return null
+    }
+
+    const visited = new Map()
+
+    const dfs = originalNode => {
+        if (visited.has(originalNode)) {
+            return visited.get(originalNode)
+        }
+
+        const clonedNode = new Node(originalNode.val)
+        visited.set(originalNode, clonedNode)
+
+        for (let neighbor of originalNode.neighbors) {
+            clonedNode.neighbors.push(dfs(neighbor))
+        }
+        return clonedNode
+    }
+    return dfs(node)
+};
+/* 
+---Breadth-First Search (BFS)--- 
+const cloneGraph = (node) => {
+    if (!node) return null
+
+    const visited = new Map()
+    const queue = [node]
+
+    const clonedNode = new Node(node.val)
+    visited.set(node, clonedNode)
+
+    while(queue.length >0) {
+        const originalNode = queue.shift()
+
+        for (let neighbor of originalNode.neighbors){
+            if (!visited.has(neighbor)) {
+                visited.set(neighbor, new Node(neighbor.val))
+                queue.push(neighbor)
+            }
+            visited.get(originalNode).neighbors.push(visited.get(neighbor))
+        }
+    }
+    return clonedNode
+}
+*/
