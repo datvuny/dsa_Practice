31d63d97e5d4c887558b2688952aace3884a2d7e
dat vu <91701889+datvuny@users.noreply.github.com>
2023-09-11 20:14:01 -0400

    add graphSchedule

--- Graphs.js
+++ Graphs.js
@@ -232,3 +232,106 @@ const pacificAtlantic = (heights) => {
     
     return result;
 };
+
+
+/////       Course Schedule (PREREQ)        /////
+/*
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+*/
+
+const canFinish = (numCourses, prerequisites) => {
+    let graph = Array(numCourses).fill(0).map(()=> [])
+    
+    for (let [course, prereq] of prerequisites) {
+        graph[course].push(prereq)
+    }
+
+    let visited = Array(numCourses).fill(0)
+
+    function hasCycle(course) {
+        if(visited[course] === 1) return true
+        if(visited[course] === 2) return false
+
+        visited[course] = 1
+
+        for (let prereq of graph[course]) {
+            if(hasCycle(prereq)) return true
+        }
+        visited[course] = 2
+        return false
+    }
+
+    for (let course = 0; course<numCourses; course++) {
+        if (hasCycle(course)) return false
+    }
+
+    return true
+};
+
+/*
+let visiting;   // is being explored
+let visited;  // is already explored
+let graph;
+
+var canFinish = function(numCourses, prerequisites) {
+    graph = new Map();
+    visiting = new Set();
+    visited = new Set();
+    
+    for(let [v, e] of prerequisites){
+        if(graph.has(v)){
+            let edges = graph.get(v);
+            edges.push(e);
+            graph.set(v,edges);
+        }else{
+            graph.set(v,[e]);
+        }
+    }
+    
+    for(const [v,e] of graph){
+        if(DFS(v)){
+            return false; //if cyclic it will not finish so it is false
+        }
+    }
+    
+    return true;
+}
+
+var DFS = function(v){
+    visiting.add(v);
+    let edges = graph.get(v);   // get all the edges to explore
+    
+    if(edges){
+        //console.log(edges)
+       for(let e of edges){
+            if(visited.has(e)){ //skip if it is explored already
+                continue;
+            }
+
+            if(visiting.has(e)){ //found e is being explored
+                return true;
+            }
+
+            if(DFS(e)){ // DFS deeper if this e is cyclic
+                return true;
+            }
+        } 
+    }   
+    
+    visiting.delete(v); // remove from visiting set when all decedant v are visited
+    visited.add(v);
+    return false;
+}
+ */
