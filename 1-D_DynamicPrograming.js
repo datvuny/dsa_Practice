0d2ad283176fd1f63dac5508dda2edfbe0f6e3cc
dat vu <91701889+datvuny@users.noreply.github.com>
2023-10-26 14:00:17 -0400

    add dsa

--- 1-D_DynamicPrograming.js
+++ 1-D_DynamicPrograming.js
@@ -42,4 +42,5 @@ security systems connected and it will automatically contact the police
 if two adjacent houses were broken into on the same night.
 Given an integer array nums representing the amount of money of each 
 house, return the maximum amount of money you can rob tonight without alerting the police.
- */
+Example 1:
+*/
