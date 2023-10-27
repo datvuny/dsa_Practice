3116efb7ff8fd2e31820619671c554419d4d1eac
dat vu <91701889+datvuny@users.noreply.github.com>
2023-10-27 14:54:01 -0400

    add dsa

--- 1-D_DynamicPrograming.js
+++ 1-D_DynamicPrograming.js
@@ -43,4 +43,5 @@ if two adjacent houses were broken into on the same night.
 Given an integer array nums representing the amount of money of each 
 house, return the maximum amount of money you can rob tonight without alerting the police.
 Example 1:
+Input: nums = [1,2,3,1]
 */
