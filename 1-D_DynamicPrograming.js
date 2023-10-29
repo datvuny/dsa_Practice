b89e386b34004ff940e6d9a2fb4aae203626c340
dat vu <91701889+datvuny@users.noreply.github.com>
2023-10-29 17:44:41 -0400

    add dsa

--- 1-D_DynamicPrograming.js
+++ 1-D_DynamicPrograming.js
@@ -45,4 +45,6 @@ house, return the maximum amount of money you can rob tonight without alerting t
 Example 1:
 Input: nums = [1,2,3,1]
 Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
 */
