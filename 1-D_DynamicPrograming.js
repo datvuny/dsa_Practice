53bff494a1492f69d58cda411581edd1624f0d73
dat vu <91701889+datvuny@users.noreply.github.com>
2023-11-02 16:58:26 -0400

    add dsa

--- 1-D_DynamicPrograming.js
+++ 1-D_DynamicPrograming.js
@@ -50,4 +50,6 @@ Total amount you can rob = 1 + 3 = 4.
 Example 2:
 Input: nums = [2,7,9,3,1]
 Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
 */
