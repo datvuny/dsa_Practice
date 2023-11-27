6892920d36d2febf328a067c06a1b72bbe35e12b
dat vu <91701889+datvuny@users.noreply.github.com>
2023-11-27 16:23:50 -0500

    add dsa

--- 1-D_DynamicPrograming.js
+++ 1-D_DynamicPrograming.js
@@ -58,6 +58,11 @@ const rob = nums => {
     const n = nums.length
     if (n === 0) return 0
     if (n === 1) return nums[0]
-
-    for
+    let dp = []
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0], nums[1])
+    for (let i=2; i<n; i++){
+      dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1])
+    }
+    
 }
